633d4e6eb398102f27192e2962b4065e95a5874b
Ariel <arielhalifa@gmail.com>
2025-12-17 12:31:20 +0200

    server 1 finished

--- server1/main.py
+++ server1/main.py
@@ -12,12 +12,16 @@ class Item(BaseModel):
     name: str
     quantity: int
 
-def load_database() -> dict:
+def load_database() -> list:
     try:
         with open("shopping_list.json", "r") as f:
             return json.load(f)
     except json.JSONDecodeError:
         raise ValueError("Database file is not valid JSON.")
+
+def save_database(data: list) -> None:
+    with open("shopping_list.json", "w") as f:
+        json.dump(data, f, indent=2)
 
 @app.get("/items")
 def list_all_items() -> None:
@@ -26,7 +30,13 @@ def list_all_items() -> None:
 
 
 @app.post("/item")
-def add_item(item:Item):
+def add_item(name, quantity):
     db_shopping_list = load_database()
     item_id = len(db_shopping_list) + 1
-    db_shopping_list
+    new_item = {"id": item_id, "name": name, "quantity": quantity}
+    db_shopping_list.append(new_item)
+    save_database(db_shopping_list)
+    return {"message": "item added succesfully",
+            "item id": item_id,
+            "new item": new_item}
+
